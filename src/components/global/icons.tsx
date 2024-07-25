c1507ffb1005dc25a53c568efda6751aa396d654
imopbuilder <imopbuilder9@gmail.com>
2024-07-25 08:16:54 +0530

    fix: import statements

--- src/components/global/icons.tsx
+++ src/components/global/icons.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
 
 export function Twitter({ className, ...props }: ComponentPropsWithoutRef<'svg'>) {
   return (
