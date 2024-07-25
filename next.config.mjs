27070cbf845586341c2416f6cf520a79f6f1d6d8
imopbuilder <imopbuilder9@gmail.com>
2024-07-25 16:54:41 +0530

    feat: add headers to cloudflare build

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://www.rahulgg.in",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "frame-ancestors 'self' https://www.rahulgg.in",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
